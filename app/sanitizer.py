2826d639727ade50b1eabfa276f861b9d0e182c9
Jordan Eldredge <jordaneldredge@gmail.com>
2014-08-18 03:33:24 -0700

    User UPPER, preserve appostrophes

--- app/sanitizer.py
+++ app/sanitizer.py
@@ -3,11 +3,11 @@ import re
 def sanitize(str):
     '''Convert a tweet string into a space delineated list of words'''
     str = re.sub(r'([a-z])([A-Z])', r'\1 \2', str)
-    str = str.lower()
     str = str.replace('@', 'at ')
     str = str.replace('#', 'hash tag ')
+    str = str.upper()
     str = re.sub(r'\s+', ' ', str)
     str = re.sub(r' $', '', str)
-    str = re.sub(r'[^a-z ]', '', str)
+    str = re.sub(r"[^A-Z ']", '', str)
     return str
 
--- app/test_sanitizer.py
+++ app/test_sanitizer.py
@@ -1,28 +1,31 @@
 from app.sanitizer import sanitize
 
 def test_sanitize_at_mentions():
-    assert sanitize('@handle') == 'at handle'
+    assert sanitize('@handle') == 'AT HANDLE'
 
 def test_sanitize_hash_tags():
-    assert sanitize('#') == 'hash tag'
+    assert sanitize('#') == 'HASH TAG'
 
 def test_expand_camel_case():
-    assert sanitize('hashTag') == 'hash tag'
+    assert sanitize('hashTag') == 'HASH TAG'
 
 def test_trim_trailing_space():
-    assert sanitize('end ') == 'end'
+    assert sanitize('end ') == 'END'
 
 def test_make_lower_case():
-    assert sanitize('Jordan Eldredge') == 'jordan eldredge'
+    assert sanitize('Jordan Eldredge') == 'JORDAN ELDREDGE'
 
 def test_remove_punctuation():
-    assert sanitize('wow!!!') == 'wow'
+    assert sanitize('wow!!!') == 'WOW'
 
 def test_contract_white_space():
-    assert sanitize('foo    bar') == 'foo bar'
+    assert sanitize('foo    bar') == 'FOO BAR'
 
 def test_contract_tabs():
-    assert sanitize('foo\tbar') == 'foo bar'
+    assert sanitize('foo\tbar') == 'FOO BAR'
 
 def test_contract_new_lines():
-    assert sanitize('foo\nbar') == 'foo bar'
+    assert sanitize('foo\nbar') == 'FOO BAR'
+
+def test_preserve_appostrophe():
+    assert sanitize("That's all") == "THAT'S ALL"
