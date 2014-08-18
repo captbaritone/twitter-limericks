e3187c0c97e7ac1788a6ee510c709a42b081cf9c
Jordan Eldredge <jordaneldredge@gmail.com>
2014-08-18 00:05:25 -0700

    Add function comment

--- app/sanitizer.py
+++ app/sanitizer.py
@@ -1,6 +1,7 @@
 import re
 
 def sanitize(str):
+    '''Convert a tweet string into a space delineated list of words'''
     str = re.sub(r'([a-z])([A-Z])', r'\1 \2', str)
     str = str.lower()
     str = str.replace('@', 'at ')
