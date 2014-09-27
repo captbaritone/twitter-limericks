c7106e957425907b9ff405208f870ca7505da85b
Jordan Eldredge <jordaneldredge@gmail.com>
2014-09-27 08:37:10 -0700

    Improve normalization

--- app/normalizer.py
+++ app/normalizer.py
@@ -5,9 +5,8 @@ def normalize(str):
     str = re.sub(r'([a-z])([A-Z])', r'\1 \2', str)
     str = str.replace('@', 'at ')
     str = str.replace('#', 'hash tag ')
-    str = str.upper()
+    str = str.lower()
     str = re.sub(r'\s+', ' ', str)
     str = re.sub(r' $', '', str)
-    str = re.sub(r"[^A-Z ']", '', str)
+    str = re.sub(r"[^a-z ']", '', str)
     return str
-
